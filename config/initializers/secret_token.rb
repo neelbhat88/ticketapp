1a2850aee9f3ab5a9e00c0dfece3293c35a13238
Neel Bhat <neelbhat88@gmail.com>
2015-01-14 21:38:32 -0600

    Initial commit of ticket app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketapp::Application.config.secret_token = 'c8168e4b0a6a4190615539f4684d2749292743eaf62ff6f050bb75013180af728bf009100d8bceab6c18adae241e41462132f27485a7b2407b2ceac7b963b6c1'
